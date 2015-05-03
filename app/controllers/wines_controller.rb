7eac3a980b908b2237cf78521f5c31833dd08b73
ajbuchanjr <ajbuchanjr@gmail.com>
2015-05-03 17:17:28 -0400

    class 2

---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,11 @@
+class WinesController < ApplicationController
+
+  def index
+    @wines = Wine.all
+  end
+
+  def show
+    @wine = Wine.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Wine.where(name:'Morgon Jean Descombes').first_or_create!(year:'2011', winery:'Georges Duboeuf', country:'France', varietal:'Gamay')
+Wine.where(name:'Brut Champagne').first_or_create!(year:'2004', winery:'Veuve Clicquot', country:'France', varietal:'Blend')
+Wine.where(name:'Hess Select Cabernet Sauvignon').first_or_create!(year:'2011', winery:'Hess', country:'USA', varietal:'Cabernet Sauvignon')
+Wine.where(name:'Montepulciano d\'Abruzzo').first_or_create!(year:'2003', winery:'Bosco', country:'Italy', varietal:'Montepulciano')
+Wine.where(name:'Sonoma Coast Pinot Noir').first_or_create!(year:'2013', winery:'Schug', country:'USA', varietal:'Pinot Noir')
+Wine.where(name:'Dona Paula Estate Malbec').first_or_create!(year:'2012', winery:'Dona Paula Estate', country:'Argentina', varietal:'Malbec')
